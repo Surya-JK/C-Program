c8b6566d9822daef02848e06ff96de2e6c8e0814
Surya JK <154452286+Surya-JK@users.noreply.github.com>
2024-03-08 06:47:26 +0530

    Add files via upload

---
+++ calculator.cc
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+ void getInputAndCalculate() 
+{
+       float num1=2, num2=3, result;
+       char operator;
+       switch (operator) 
+      {
+           case '+':
+               result = num1 + num2;
+               break;
+           case '-':
+               result = num1 - num2;
+               break;
+           case '*':
+               result = num1 * num2;
+               break;
+           case '/';
+               if (num2 != 0) 
+               {
+                   result = num1 / num2;
+                   printf("Result: %.2f\n", result);
+               } 
+               else 
+              {
+                   printf("Error: Division by zero.\n");
+               }
+               break;
+           default:
+               printf("Error: Invalid operator.\n");
+       }
+   }
+
+   int main()
+ {
+       getInputAndCalculate();
+       return 0;
+   }
